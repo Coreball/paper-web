2d0a45ad6d07210a73d9eccb64b46bd31c831553
Changyuan Lin <coreballium@outlook.com>
2023-05-19 04:24:31 -0400

    No more flashing on edit to send dialog

--- src/EditSendDialog.tsx
+++ src/EditSendDialog.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
   Button,
   Dialog,
@@ -27,9 +27,7 @@ export const EditSendDialog = ({
   const [sendDialogOpen, setSendDialogOpen] = useState(false)
   const [sendHeading, setSendHeading] = useState(0)
   const sendPlane = () => {
-    setEditDialogOpen(true)
-    setSendDialogOpen(false)
-    if (userCenter) {
+    if (canSendPlane) {
       handleAddPlane({
         origin: userCenter,
         heading: sendHeading,
@@ -39,69 +37,76 @@ export const EditSendDialog = ({
   }
   const canSendPlane = userCenter !== null
 
-  const active = currentPlane || creatingNewPlane
+  const active = currentPlane !== null || creatingNewPlane
+  // useEffect to reset the dialog whenever changes to active
+  useEffect(() => {
+    if (active) {
+      setEditDialogOpen(true)
+      setSendDialogOpen(false)
+    }
+  }, [active])
+
   const handleNext = () => {
     setEditDialogOpen(false)
     setSendDialogOpen(true)
     setSendHeading(0)
   }
-  const handleCancel = () => {
-    setEditDialogOpen(true)
-    setSendDialogOpen(false)
-    handleCancelPlane()
-  }
 
   return (
-    <>
-      <Dialog open={active && editDialogOpen} onClose={handleCancel}>
-        <DialogTitle>Edit Plane</DialogTitle>
-        <DialogContent>
-          <DialogContentText>TODO edit plane interface</DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCancel}>Cancel</Button>
-          <Button onClick={handleNext}>Next</Button>
-        </DialogActions>
-      </Dialog>
-      <Dialog open={active && sendDialogOpen} onClose={handleCancel}>
-        <DialogTitle>Send Plane</DialogTitle>
-        <DialogContent
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            gap: 4,
-          }}
-        >
-          {canSendPlane ? (
-            <>
-              <DialogContentText>
-                Choose an initial plane heading
-              </DialogContentText>
-              <Send
-                fontSize="large"
-                sx={{ transform: `rotate(${sendHeading - 90}deg)` }}
-              />
-              <Typography mb={-2}>{sendHeading}</Typography>
-              <Slider
-                min={-180}
-                max={180}
-                value={sendHeading}
-                onChange={(_, val) => setSendHeading(val as number)}
-              />
-            </>
-          ) : (
-            <DialogContentText>Need your location :(</DialogContentText>
-          )}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCancel}>Cancel</Button>
-          <Button disabled={!canSendPlane} onClick={sendPlane}>
-            Send
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </>
+    <Dialog open={active} onClose={handleCancelPlane}>
+      {editDialogOpen && (
+        <>
+          <DialogTitle>Edit Plane</DialogTitle>
+          <DialogContent>
+            <DialogContentText>TODO edit plane interface</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCancelPlane}>Cancel</Button>
+            <Button onClick={handleNext}>Next</Button>
+          </DialogActions>
+        </>
+      )}
+      {sendDialogOpen && (
+        <>
+          <DialogTitle>Send Plane</DialogTitle>
+          <DialogContent
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              gap: 4,
+            }}
+          >
+            {canSendPlane ? (
+              <>
+                <DialogContentText>
+                  Choose an initial plane heading
+                </DialogContentText>
+                <Send
+                  fontSize="large"
+                  sx={{ transform: `rotate(${sendHeading - 90}deg)` }}
+                />
+                <Typography mb={-2}>{sendHeading}</Typography>
+                <Slider
+                  min={-180}
+                  max={180}
+                  value={sendHeading}
+                  onChange={(_, val) => setSendHeading(val as number)}
+                />
+              </>
+            ) : (
+              <DialogContentText>Need your location :(</DialogContentText>
+            )}
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCancelPlane}>Cancel</Button>
+            <Button disabled={!canSendPlane} onClick={sendPlane}>
+              Send
+            </Button>
+          </DialogActions>
+        </>
+      )}
+    </Dialog>
   )
 }
 
